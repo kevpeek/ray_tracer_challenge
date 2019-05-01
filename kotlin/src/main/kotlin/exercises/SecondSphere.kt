94f9f010595e565cbae4ed9677dd1afb2f241c32
Kevin Peek <kevpeek@gmail.com>
2019-05-01 19:30:43 -0400

    render first sphere with reflection

---
+++ kotlin/src/main/kotlin/exercises/SecondSphere.kt
@@ -0,0 +1,69 @@
+package exercises
+
+import display.Canvas
+import display.Color
+import display.canvasToPpm
+import display.writePpm
+import geometry.Matrix
+import geometry.Point
+import geometry.Vector
+import geometry.WORLD_ORIGIN
+import geometry.rotationX
+import geometry.rotationY
+import geometry.rotationZ
+import geometry.scaling
+import geometry.translation
+import tracing.Material
+import tracing.PointLight
+import tracing.Ray
+import tracing.Sphere
+import tracing.hit
+import tracing.intersects
+import tracing.lighting
+
+
+private fun getShape(): Sphere {
+    val material = Material(color = Color(1, 0.2, 1))
+    val transformation = Matrix.identity(4)
+    return Sphere(transform = transformation, material = material)
+}
+
+private fun getLight(): PointLight {
+    val lightPosition = Point(-10, 10, -10)
+    val lightColor = Color.WHITE
+    return PointLight(lightPosition, lightColor)
+}
+
+
+fun main() {
+    val shape = getShape()
+    val light = getLight()
+
+    val canvasPixels = 100
+    val canvas = Canvas(canvasPixels, canvasPixels)
+    val rayOrigin = Point(0, 0, -5)
+    val wallZ = 10
+    val wallSize = 7.0
+    val pixelSize = wallSize / canvasPixels
+    val half = wallSize / 2
+
+    getCanvasPoints(canvasPixels).forEach { (x, y) ->
+        val worldY = half - pixelSize * y
+        val worldX = -half + pixelSize * x
+        val position = Point(worldX, worldY, wallZ)
+        val rayDirection = (position - rayOrigin).normalize()
+        val ray = Ray(rayOrigin, rayDirection)
+        val intersections = intersects(shape, ray)
+        hit(intersections)?.let {hit ->
+            val point = ray.position(hit.time)
+            val normal = hit.thing.normalAt(point)
+            val eye = -(ray.direction)
+            val color = lighting(shape.material, light, point, eye, normal)
+            canvas.writePixel(x, y, color)
+        }
+    }
+
+    writePpm(canvasToPpm(canvas))
+}
+
+private fun getCanvasPoints(canvasSize: Int) = (0 until canvasSize).flatMap { y -> (0 until canvasSize).map {x -> x to y}}
--- kotlin/src/main/kotlin/tracing/Material.kt
+++ kotlin/src/main/kotlin/tracing/Material.kt
@@ -8,24 +8,26 @@ fun lighting(material: Material, light: PointLight, position: Point, eyeVector: 
     val ambient = ambientContribution(material, light)
     val diffuse = diffuseContribution(material, light, position, normal)
     val specular = specularContribution(material, light, position, eyeVector, normal)
-    println("ambient: $ambient, diffuse: $diffuse, specular: $specular")
     return ambient + diffuse + specular
 }
 
 private fun ambientContribution(material: Material, light: PointLight): Color {
-    val effectiveColor = material.color * light.intensity
+    val effectiveColor = effectiveColor(material, light)
     return effectiveColor * material.ambient
 }
 
 private fun diffuseContribution(material: Material, light: PointLight, position: Point, normal: Vector): Color {
-    val effectiveColor = material.color * light.intensity
     val lightDirection = (light.position - position).normalize()
     val lightDotNormal = lightDirection.dot(normal)
     return if (lightDotNormal < 0) {
         Color.BLACK
     } else {
-        effectiveColor * material.diffuse * lightDotNormal
+        effectiveColor(material, light) * material.diffuse * lightDotNormal
     }
+}
+
+private fun effectiveColor(material: Material, light: PointLight): Color {
+    return material.color * light.intensity
 }
 
 private fun specularContribution(material: Material, light: PointLight, position: Point, eyeVector: Vector, normal: Vector): Color {
@@ -46,12 +48,13 @@ private fun specularContribution(material: Material, light: PointLight, position
     }
 }
 
-class Material {
-    val color = Color(1, 1, 1)
-    val ambient = 0.1
-    val diffuse = 0.9
-    val specular = 0.9
-    val shininess = 200.0
+class Material(
+    val color: Color = Color.WHITE,
+   val ambient: Double = 0.1,
+   val diffuse: Double = 0.9,
+   val specular: Double = 0.9,
+   val shininess: Double = 200.0
+) {
 
     companion object {
         val DEFAULT = Material()
