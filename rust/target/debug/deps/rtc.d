b5a92c7021c999fd51c0c52cb3b5d1ce8a738a3f
Kevin Peek <kevpeek@gmail.com>
2021-07-11 10:58:07 -0400

    init a rust project

---
+++ rust/target/debug/deps/rtc.d
@@ -0,0 +1,5 @@
+/Users/kevin/Code/ray_tracer_challenge/rust/target/debug/deps/rtc: src/main.rs
+
+/Users/kevin/Code/ray_tracer_challenge/rust/target/debug/deps/rtc.d: src/main.rs
+
+src/main.rs:
---
+++ rust/target/debug/rtc.d
@@ -0,0 +1,1 @@
+/Users/kevin/Code/ray_tracer_challenge/rust/target/debug/rtc: /Users/kevin/Code/ray_tracer_challenge/rust/src/main.rs
